43d37f0605c0a159981df3a67ec0a3b2651b360e
想要成为RapStar吗？ <hanqiulun1123@163.com>
2020-09-21 15:04:41 +0800

    feat：增加新的models逻辑

--- app/src/main/java/com/lateautumn4lin/headwolf/models/kuaishou/Entry.java
+++ app/src/main/java/com/lateautumn4lin/headwolf/models/kuaishou/Entry.java
@@ -1,28 +1,17 @@
-package com.lateautumn4lin.headwolf.models;
+package com.lateautumn4lin.headwolf.models.kuaishou;
 /*
- * Kuaishou
+ * Entry
  *
  * @author lateautumn4lin
  * @github https://github.com/lateautumn4lin
- * @date 2020/9/21 14:07
+ * @date 2020/9/21 15:01
  */
 
 import com.lateautumn4lin.headwolf.commons.Logger;
 
-/**
- * The type Kuaishou v 2.
- */
-public class Kuaishou_v2 {
-    /**
-     * The constant classLoader.
-     */
+public class Entry {
     public static ClassLoader classLoader = null;
 
-    /**
-     * Init.
-     *
-     * @param classloader the classloader
-     */
     public static void init(ClassLoader classloader) {
         classLoader = classloader;
         Logger.logi(classloader.toString());
